7e0b5b5d80ab911732133072cc570bd9056762a9
CJ Davis <carliebug812@gmail.com>
2025-06-01 15:17:57 -0500

    9

--- resources/java/script.js
+++ resources/java/script.js
@@ -19,3 +19,31 @@ for (i=1; i<14; i++){
         cards.push(j);
     }
 }
+
+cards.shuffle = function() {
+    console.log("shuffle");
+    var input = this;
+    for (var i = cards.length-1; i >=0; i--) {
+        var randomIndex = Math.floor(Math.random()*(i+1));
+        var itemAtIndex = cards[randomIndex][0];
+        var itemAtSecond = cards[randomIndex][1];
+        input[randomIndex][0] = input[i][0];
+        input[randomIndex][1] = input[i][0];
+        input[i][0] = itemAtIndex;
+        input[i][1] = itemAtSecond;
+    }
+    return input;
+}
+
+cards.shuffle();
+
+var half = cards.length/2;
+for (i=0; i<half; i++) {
+    firstPlayer.push(cards[i]);
+}
+
+cards.splice(0, half);
+
+secondPlayer = cards;
+$player1Count.html(firstPlayer.length);
+$player2Count.html(secondPlayer.length);
